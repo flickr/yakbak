f7ce7337e08c95cff31cf1b611fe4f8e104346a8
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2016-04-25 14:41:45 -0700

    1.5.0 compatibility

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -1,6 +1,7 @@
 // Copyright 2016 Yahoo Inc.
 // Licensed under the terms of the MIT license. Please see LICENSE file in the project root for terms.
 
+var semver = require('semver');
 var path = require('path');
 var fs = require('fs');
 
@@ -8,4 +9,14 @@ function read(file) {
   return fs.readFileSync(path.join(__dirname, file + '.js'), 'utf8');
 }
 
-module.exports = read('v0.10.x');
+/**
+ * node >= 1.5.0 sends the content-length whenever possible
+ * @see https://github.com/nodejs/node/pull/1062
+ */
+
+if (semver.gte(process.version, '1.5.0')) {
+  console
+  module.exports = read('v1.5.0');
+} else {
+  module.exports = read('v0.10.x');
+}
